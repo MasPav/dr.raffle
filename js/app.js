a3cceb04ad3d5d24451c40b9bd6bebc907d772da
Cyprian Ampong Boateng <caboateng@IM-21-B1-036.local>
2021-03-05 19:32:57 +0000

    working on wheel

---
+++ js/app.js
@@ -0,0 +1,88 @@
+let theWheel;
+const raffleData = [{
+        name: 'Akwasi Boateng'
+    },
+    {
+        name: 'Benjamin Acheampong'
+    },
+    {
+        name: 'Daniel Twumasi'
+    },
+    {
+        name: 'Ford Prior'
+    },
+    {
+        name: 'Jerry Agudogo'
+    },
+    {
+        name: 'Kwaku Appiah- Adu'
+    },
+    {
+        name: 'NATHANIEL NAT'
+    },
+    {
+        name: 'Nkwenti Fon'
+    },
+    {
+        name: 'Yusuf'
+    },
+    {
+        name: 'Vincent Quainoo'
+    },
+    {
+        name: 'Steve Kamanke'
+    },
+    {
+        name: 'Edem'
+    },
+    {
+        name: 'Olena'
+    },
+    {
+        name: 'Nii Akai'
+    }
+];
+$(function () {
+    initWinWheel();
+    /** Settings */
+    // raffle title
+    $('#showRaffleTitleSwitch').on('change', function () {
+        document.querySelector('#raffleTitleInput').toggleAttribute('disabled');
+        $('#raffleTitle').toggle();
+    });
+    $('#raffleTitleInput').on('keyup', function () {
+        $('#raffleTitle').text($(this).val())
+    })
+});
+const initWinWheel = () => {
+    theWheel = new Winwheel({
+        canvasId: 'raffle-canvas',
+        numSegments: raffleData.length,
+        lineWidth: '3',
+        responsive: true,
+        segments: raffleData,
+        textFontFamily: 'sans-serif',
+        textAlignment: 'outer',
+        textFontWeight: 'bold',
+        innerRadius: 40,
+        // Note animation properties passed in constructor parameters.
+        animation: {
+            type: 'spinToStop', // Type of animation.
+            duration: 5, // How long the animation is to take in seconds.
+            spins: 10, // The number of complete 360 degree rotations the wheel is to do.
+            easing: 'Power4.easeInOut',
+            // Remember to do something after the animation has finished specify callback function.
+            callbackFinished: 'onSpinStopped()'
+
+            // During the animation need to call the drawTriangle() to re-draw the pointer each frame.
+        },
+        pointerAngle: 0,
+        // Turn pointer guide on.
+        pointerGuide: {
+            display: false,
+            strokeStyle: 'red',
+            lineWidth: 3,
+            textMargin: 0
+        }
+    });
+};
